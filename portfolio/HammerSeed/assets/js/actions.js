d5b07342a4a8a31f21e5105e4228cb747df9a848
Monty Rasmussen <montyr75@gmail.com>
2023-06-29 16:00:34 -0600

    resume update

--- portfolio/HammerSeed/assets/js/actions.js
+++
@@ -1,8 +0,0 @@
-define(['angular', 'utils/cache'], function (angular, cache) {
-    'use strict';
-
-    cache('actions').set('windowAlert', function (scope, action) {
-        window.alert(action.message);
-    });
-
-});
--- portfolio/HammerSeed/assets/js/controllers/myController.js
+++
@@ -1,5 +0,0 @@
-define(['angular'], function (angular) {
-    'use strict';
-//    angular.module('app').controller('MyController', function ($scope) {
-//    });
-});
